bb17ceb9038d377cf28e46ec6d14ae67f74800f2
Edgar Briandt <99098876+Merxibeaucoup@users.noreply.github.com>
2022-11-18 11:38:21 -0500

    count

---
+++ src/java_8_counting/Counting.java
@@ -0,0 +1,32 @@
+package java_8_counting;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Counting {
+
+	public static void main(String[] args) {
+		
+			
+		
+		/**
+		 * count the number of times each item appears
+		 */
+		
+		
+		List<String> items = Arrays.asList(
+				"apple", "apple", "melon", "apple", "apple", "melon", "banana", "apple",
+				"melon", "orange", "orange", "banana", "melon", "papaya", "melon", "lemon");
+		
+		Map<String, Long> count = items
+									.stream()
+									.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+		
+		System.out.println(count);
+
+	}
+
+}
