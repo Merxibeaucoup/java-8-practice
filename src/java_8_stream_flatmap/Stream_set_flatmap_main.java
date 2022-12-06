683f7032bf5025f7841eafffcd71ac9155dee108
Edgar Briandt <99098876+Merxibeaucoup@users.noreply.github.com>
2022-12-05 21:01:54 -0500

    flatmap done

--- src/java_8_stream_flatmap/Stream_set_flatmap_main.java
+++ src/java_8_stream_flatmap/Stream_set_flatmap_main.java
@@ -2,6 +2,7 @@ package java_8_stream_flatmap;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Stream_set_flatmap_main {
 
@@ -31,6 +32,35 @@ public class Stream_set_flatmap_main {
 		List<Student> list = new ArrayList<>();
 		list.add(student1);
 		list.add(student2);
+		
+		
+		//output
+		//Java Developer [JavaFX, Hibernate, Swing, Spring Framework, Spring Boot, Java 8 in Action, SQL]
+		//Python Developer [Django, Learning Python, Numpy, SQL]
+		list.forEach(x -> System.out.println(x.getType() + " " + x.getBook()));
+		
+		System.out.println("--------------------------------");
+		
+		List<String> collect = list.stream()
+				.map(x -> x.getBook()) // Stream<Set<String>>
+				.flatMap(x -> x.stream()) // Stream<String>
+				.distinct() // unique books occurrence
+				.collect(Collectors.toList());
+		
+		
+		//output
+		// JavaFX, Hibernate, Swing, Spring Framework, Spring Boot, Java 8 in Action, SQL, Django, Learning Python, Numpy, 
+		collect.forEach(x -> System.out.print(x + ", "));
+		System.out.println("\n--------------------------------");
+		
+		list.get(0).getBook().forEach(x -> System.out.println(x));	// get all books for Java_Developer
+		
+		System.out.println("\n--------------------------------");
+
+		
+		list.get(1).getBook().forEach(x -> System.out.println(x));	// get all books for Python_Developer
+		
+		
 
 	}
 
